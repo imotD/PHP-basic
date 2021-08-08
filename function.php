e841e9afc3342e82b416326fddce2ad3934995e3
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-08-08 09:42:53 +0700

    add function

---
+++ function.php
@@ -0,0 +1,27 @@
+<?php
+//? menampilkan tanggal dengan format terntentu
+// echo date("l , d-M-Y </br>");
+//? UNiX timestamp
+// echo time()
+function salam($waktu = "Datang", $nama = "Admin")
+{
+    return "Selamat $waktu, $nama!";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Function</title>
+</head>
+
+<body>
+    <h1><?= salam(); ?></h1>
+</body>
+
+</html>
